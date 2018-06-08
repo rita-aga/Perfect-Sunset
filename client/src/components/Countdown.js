29c761094eb9c39c108a9305460c910b91e2aa57
Rita Agafonova <ritaagafonova@Rita-Agafonovas-MacBook-Pro.local>
2018-06-08 00:52:40 -0400

    timer crash fix

--- client/src/components/Countdown.js
+++ client/src/components/Countdown.js
@@ -43,8 +43,9 @@ componentDidUpdate() {
       <div>
       {/* {this.props.target && <p>Target time is {this.props.target}</p>} */}
     {/* <p>Current time: {this.state.hours}:{this.state.minutes}:{this.state.seconds}</p> */}
-    {this.state.secondsRemaining > 0 && <p className="count"><img src="img/sunset.svg"/>{this.pad(this.state.hoursRemaining)}:{this.pad(this.state.minutesRemaining)}:{this.pad(this.state.secondsRemaining)}</p>}
-    {this.state.secondsRemaining < 0 && <p className="count"><img src="img/sunset.svg"/>{this.pad(Math.abs(this.state.hoursRemaining))}:{this.pad(Math.abs(this.state.minutesRemaining))}:{this.pad(Math.abs(this.state.secondsRemaining))}</p>}
+    {/* <img src="img/sunset.svg"/> */}
+    {this.state.secondsRemaining > 0 && <p className="count">{this.pad(this.state.hoursRemaining)}:{this.pad(this.state.minutesRemaining)}:{this.pad(this.state.secondsRemaining)}</p>}
+    {this.state.secondsRemaining < 0 && <p className="count">{this.pad(Math.abs(this.state.hoursRemaining))}:{this.pad(Math.abs(this.state.minutesRemaining))}:{this.pad(Math.abs(this.state.secondsRemaining))}</p>}
       </div>
     )
   }
